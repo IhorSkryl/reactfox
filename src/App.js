3eac7995390d29f3e43b762c92d52869fe6bcd64
Ihor Skryl <ihorskryl@MacBook-Pro-Ihor.local>
2021-07-17 15:46:22 +0300

    this is test commit to check if my ssh setup is working fine

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
     <div className="app">
       <div className="app__container">
         <h2>Choose link above</h2>
+        <h2>Choose link above</h2>
       </div>
     </div>
   );
